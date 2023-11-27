4ad184c17f54e21a582ee4ae536b39c3837651c8
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-11-27 20:21:52 -0300

    server/add swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger/dist'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Git commit history API')
+    .setVersion('1.0')
+    .addTag('commits')
+    .build()
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('', app, document)
   await app.listen(3000)
 }
 bootstrap()
