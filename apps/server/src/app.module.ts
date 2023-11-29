be8de58e17adb6beb20ef0c727dbafb008b5d19f
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-11-29 11:25:29 -0300

    add configuration

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CommitsModule } from './commits/commits.module'
+import config from 'config/config'
 
 @Module({
-  imports: [CommitsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config]
+    }),
+    CommitsModule
+  ],
   controllers: [],
   providers: []
 })
