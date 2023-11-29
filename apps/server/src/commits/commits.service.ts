b519f34a137471eba0ca52c043bf9e5d2d50ff88
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-11-29 11:50:08 -0300

    add option to pass custom repo data

--- apps/server/src/commits/commits.service.ts
+++ apps/server/src/commits/commits.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { OctokitClient } from 'src/octokit/octokitClient'
 
 @Injectable()
 export class CommitsService {
-  constructor(private readonly octokit: OctokitClient) {}
+  constructor(private readonly octokit: OctokitClient, private readonly configService: ConfigService) {}
   async getAllCommits() {
     return this.octokit.getClient().request('GET /repos/{owner}/{repo}/commits', {
-      owner: 'SantiagoJavierRubio',
-      repo: 'git-commit-history'
+      owner: this.configService.get<string>('repo.owner'),
+      repo: this.configService.get<string>('repo.name')
     })
   }
 }
